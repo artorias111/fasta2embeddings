16897fbdb7b42fbb4e9d5eaa61091562c6cde6bd
Shriram <alekhshriram@gmail.com>
2025-12-15 14:27:13 -0600

    helper scripts

---
+++ bin/fasta2embeddings.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python3
+
+import torch
+import torch.nn as nn
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+
+# from evo2 import Evo2
+# evo2_model = Evo2('evo2_7b')
+
+le = LabelEncoder()
+le.fit(['A', 'T', 'G', 'C', 'N'])
+
+def tokenize_fasta(path, k_size): # returns {header: [k-mers]} right now. Goal is to return {header: [embeddigns]}
+    d = {}
+    with open(path) as file:
+        header = ''
+        sequence = ''
+        is_header = 0
+        for line in file:
+            l = line.strip()
+            if l[0] == '>':
+                header = l
+                is_header = 0
+            else:
+                is_header = 1
+
+            if is_header = 1
+                sequence += l
+        d[header] = stream_kmers(sequence, k_size)
+    return d
+
+
+def stream_kmers(sequence, k_size): # doesn't stream k-mers yet, right now it saves k-mers in a list and returns the list
+    k_mers_list = []
+    for index in range(0, len(sequence)-k_size+1):
+        k-mers_list.append(sequence[index:index+k_size])
+
+    return k_mers_list
+
+def k_mer2embed(k_mer): # return embedding for a single k-mer
+    label_encodings = le.transform(list(k-mer))
+
+
+def revcomp(nucl): # take a nucleotide and return its reverse complement
+    revc = ''
+    for i in nucl[::-1]:
+        revc += COMP[i]
+    return revc
+
+

9a36ac90e07431dc0c847f39d2c5faf82bc578f1
Shriram <alekhshriram@gmail.com>
2025-12-17 12:19:31 -0600

    embeddings in the future. Encodings for now

---
+++ bin/fasta2encodings.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+from sklearn.preprocessing import LabelEncoder
+
+class encodeFasta:
+    def __init__(path, k_size):
+        self.path = path
+        self.k_size = k_size
+
+    COMP = {'A':'T', 'T':'A', 'C':'G', 'G': 'C', 'N':'N'}
+    le = LabelEncoder()
+    le.fit(['A', 'T', 'G', 'C', 'N'])
+
+    def tokenize_fasta(path, k_size): # Goal is to return {header: [embeddings]}
+        d = {}
+        with open(path) as file:
+            header = ''
+            sequence = ''
+            is_header = 0
+            for line in file:
+                l = line.strip()
+                if l[0] == '>':
+                    header = l
+                    d[header] = []
+                    is_header = 0
+                else:
+                    is_header = 1
+
+                if is_header == 1:
+                    sequence += l
+            d[header] = d[header].append(stream_kmer_encodings(sequence, k_size))
+        return d
+
+
+    def stream_kmer_encodings(sequence, k_size): # doesn't stream k-mers yet, right now it saves k-mers in a list and returns the list
+        for index in range(0, len(sequence)-k_size+1):
+            encoding = encode_kmer(canonical(sequence[index:index+k_size]))
+            yield encoding
+
+    def encode_kmer(k_mer): # return embedding for a single k-mer
+        label_encodings = le.transform(list(k_mer))
+        return label_encodings
+
+    def canonical(sequnece): # take a sequence, and return its caonical form
+        revc = revcomp(sequence)
+        if revc > sequence:
+            return sequence
+        return revc
+
+    def revcomp(nucl): # take a nucleotide and return its reverse complement
+        revc = ''
+        for i in nucl[::-1]:
+            revc += COMP[i]
+        return revc
